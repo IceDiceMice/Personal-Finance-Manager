f230ed9326769099f052606c5f9868280c390292
Dima Olyinyk <prototupe@ukr.net>
2021-10-30 12:45:54 +0300

    some configs

--- client/src/components/partials/Navbar.js
+++ client/src/components/partials/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import { BsJustify } from "react-icons/bs";
 import Dropdown from "./Dropdown";
--- server.js
+++ server.js
@@ -1,25 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose');
-require('dotenv').config();
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
+if (process.env.NODE_ENV === production) {
+  app.use(express.static("client/build"));
+}
 
-
-const categoryRouter = require("./routes/category-route")
-const operationsRouter = require("./routes/operations-route")
+const categoryRouter = require("./routes/category-route");
+const operationsRouter = require("./routes/operations-route");
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json())
+app.use(express.json());
 
 app.use("/category", categoryRouter);
 app.use("/operations", operationsRouter);
 
-mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true })
-.then(()=> console.log('Connected to DB'))
-.catch((error)=>console.log(error))
+mongoose
+  .connect(process.env.MONGO_URL, { useNewUrlParser: true })
+  .then(() => console.log("Connected to DB"))
+  .catch((error) => console.log(error));
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on Port: ${process.env.PORT}.`);
+  console.log(`Server is running on Port: ${process.env.PORT}.`);
 });
