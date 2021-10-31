8247279c6b9af7a35aac0fce28f372d004eacede
Dima Olyinyk <prototupe@ukr.net>
2021-10-31 14:16:22 +0200

    some changed

--- server.js
+++ server.js
@@ -3,10 +3,19 @@ const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const path = require("path");
 
-if (process.env.NODE_ENV === production) {
-  app.use(express.static("client/build"));
-}
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+} else
+  app.get("/", (req, res) => {
+    res.send("API is running");
+  });
+
+const PORT = process.env.PORT || 4000;
 
 const categoryRouter = require("./routes/category-route");
 const operationsRouter = require("./routes/operations-route");
@@ -23,6 +32,6 @@ mongoose
   .then(() => console.log("Connected to DB"))
   .catch((error) => console.log(error));
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on Port: ${process.env.PORT}.`);
+app.listen(PORT, () => {
+  console.log(`Server is running on Port: ${PORT}.`);
 });
